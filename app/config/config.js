f01356f1daf3bec91d984473d1831248b8a5ce91
Anik Deb <anik.deb161@gmail.com>
2025-01-21 01:37:12 +0600

    Edited Config

---
+++ app/config/config.js
@@ -0,0 +1,27 @@
+export const JWT_SECRET = "1e87520236581a9175d6986010cb416b4ec3fdffd437a7dad12bec30907c1cee";
+export const JWT_EXPIRATION_TIME = 60*60*24*30;
+
+
+export const MAX_JSON_SIZE = "50mb";
+export const URL_ENCODER = true;
+
+export const REQUEST_LIMIT_TIME = 15*60*1000; //15 Min
+export const REQUEST_LIMIT_NUMBER = 3000; // Per 15 Min 3000 Request Allowed
+
+export const WEB_CACHE = false; 
+
+export const PORT = 5050;
+
+//For Google authentication(SignUp with Google)
+export const GOOGLE_CLIENT_ID="";
+export const GOOGLE_CLIENT_SECRET="";
+
+//MongoDB Connection
+export const MONGODB_CONNECTION = "";
+
+//Set Email Server Info to work Reset Password Email Verification Perfectly
+export const EMAIL_HOST = "";
+export const EMAIL_PORT = "";
+export const EMAIL_USER = "";
+export const EMAIL_PASSWORD = "";
+export const MAIL_ENCRYPTION = "";
